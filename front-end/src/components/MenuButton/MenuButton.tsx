6e18cd493322063b87d36700f27f7911456cab2e
joseCarlosAndrade <josecarlos.101@usp.br>
2024-07-30 11:44:08 -0300

    feat: botao do menu exportado do figma

--- front-end/src/components/MenuButton/MenuButton.tsx
+++ front-end/src/components/MenuButton/MenuButton.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './MenuButton.css';
 
+import { MenuIconSVG } from 'src/assets';
+
 function MenuButton() {
   return (
-    <button className="menu-button">
-      <div className="bar"></div>
-      <div className="bar"></div>
-      <div className="bar"></div>
-    </button>
-  );
+    <div className='menu-button'>
+      <img src={MenuIconSVG.toString()} alt="" />
+    </div>
+  )
+  // return (
+  //   <button className="menu-button">
+  //     <div className="bar"></div>
+  //     <div className="bar"></div>
+  //     <div className="bar"></div>
+  //   </button>
+  // );
 }
 
 export default MenuButton;
