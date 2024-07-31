986c1195550e3ba074c0b1a66321340448b32fc9
Camila Donda <camila.donda@usp.br>
2024-07-31 13:50:00 -0300

    feature: página de presença individual finalizada

--- front-end/src/pages/PresencaIndividual/PresencaIndividual.tsx
+++ front-end/src/pages/PresencaIndividual/PresencaIndividual.tsx
@@ -1,14 +1,24 @@
 import CardPorcentagemPresenca from "src/components/CardPorcentagemPresenca/CardPorcentagemPresenca"
 import './PresencaIndividual.css'
+import { useLocation } from 'react-router-dom';
+import CardFotoUsuario from "src/components/CardFotoUsuario/CardFotoUsuario"
+
 
 function PresencaIndividual(){
+
+    const location = useLocation();
+    const mensagem = location.state?.mensagem || "";
 
     return(
         <div className="Presenca-individual">
 
             <div className="Presenca-individual-dados">
+                <CardFotoUsuario
+                    nome={"Lucas Piovani Ferreira"}
+                    cargo={"Membro"}
+                />
                 <div className="Presenca-individual-dados-titulo">
-                    <h3>Presenças:</h3>
+                    <h2>Presenças:</h2>
                 </div>
                 <div className="Presenca-individual-dados-card">
                     <CardPorcentagemPresenca
@@ -16,6 +26,9 @@ function PresencaIndividual(){
                         presenca={9}
                         falta={0}
                     />
+                </div>
+                <div className="Presenca-individual-mensagem">
+                    <h5>{mensagem}</h5>
                 </div>
                 
             </div>
